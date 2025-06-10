2af6b8b6d2b2984dc4d04f2be23c0b96c042fe81
Nursan amar <nursan011@gmail.com>
2025-06-11 06:42:53 +0800

    chore: setup github action

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -6,5 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
-    }
+    },
+    site: 'https://nursanamar.com',
+    base: 'blog',
 });
